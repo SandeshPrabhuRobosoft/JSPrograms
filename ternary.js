0c72afacb51eec9517cf91f63d834a0d005d3ce7
SandeshPrabhuRobosoft <111508194+SandeshPrabhuRobosoft@users.noreply.github.com>
2022-08-29 09:44:15 +0530

    Update ternary.js
    added a console log

--- ternary.js
+++ ternary.js
@@ -2,3 +2,6 @@ function largest_num_of_3(number1,number2,number3){
     return (number1>number2) ? ((number1>number3) ? number1 : number3) : ((number2>number3) ? number2 : number3);
 }
 console.log(largest_num_of_3(21,10,7));
+
+
+console.log("its me sandesh")
